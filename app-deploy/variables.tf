6238659645e03e61262ff7a5a2038d819195d033
kmusikowski <56798609+Kameroi@users.noreply.github.com>
2022-11-28 11:27:06 +0100

    zz

---
+++ app-deploy/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS provider region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "vpc_name" {
+  description = "Name of a VPC to be created"
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "cluster_name" {
+  description = "K8S cluster name"
+  type        = string
+  default     = "my-cluster"
+}
